4a940fe35b6c4d597d198ef5d013f4f5de26cd20
PramodiThilakarathne <pramodithilakarathne@gmail.com>
2023-08-11 02:30:48 +0530

    check

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/add_book', function () {
 })->name('addbook');
 Route::get('/view_book', function () {
     return view('Admin_Dashboard.View_Booking');
-})->name('view_book');*/
+})->name('view_book');
 
 
 Route::get('/', function () {
