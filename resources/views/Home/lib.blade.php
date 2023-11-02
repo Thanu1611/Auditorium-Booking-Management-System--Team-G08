f952a0e408f54e5e6f9fc2653bec550fda8c3fb6
PramodiThilakarathne <pramodithilakarathne@gmail.com>
2023-11-02 11:42:29 +0530

    welcome

--- resources/views/Home/lib.blade.php
+++ resources/views/Home/lib.blade.php
@@ -25,13 +25,13 @@
 
         <div class="carousel-inner">
           <div class="carousel-item " data-bs-interval="2000">
-          <img src="/images/lib2.jpg" class="d-block w-100" alt="..." height="600">
+          <img src="/images/L1.jpg" class="d-block w-100" alt="..." height="600">
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-          <img src="/images/lib.jpg" class="d-block w-100" alt="..." height="600">
+          <img src="/images/L2.jpg" class="d-block w-100" alt="..." height="600">
           </div>
           <div class="carousel-item active" data-bs-interval="2000">
-          <img src="/images/liib 1.JPG" class="d-block w-100" alt="..." height="600">
+          <img src="/images/L3.JPG" class="d-block w-100" alt="..." height="600">
           </div>
           
         </div>
